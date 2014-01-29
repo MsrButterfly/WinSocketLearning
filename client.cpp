d9df4d212c54f55f2da3cc72eb5046b3ea75320f
MSR-B <liuhongzhe19940501@gmail.com>
2014-01-30 06:05:43 +0800

    Added code files.

---
+++ client.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <thread>
+#include <cstring>
+#include <windows.h>
+#include <process.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using Socket          = SOCKET;
+using SocketAddress   = SOCKADDR;
+using SocketAddressIn = SOCKADDR_IN;
+using Word            = WORD;
+inline Word makeWord(const int &a, const int &b) {
+    return MAKEWORD(a, b);
+}
+const uint64_t ServerIP      = 0xC0A80106; // or const char *ServerIP = 192.168.1.6, with inet_addr(ServerIP)
+const uint32_t ServerPort    = 10000;
+const uint32_t MaxDataLength = MAX_PATH;
+
+int main(int argc, const char *argv[]) {
+    WSAData wsaData;
+    const Word versionRequested = makeWord(2, 2);
+    if (WSAStartup(versionRequested, &wsaData) != 0) {
+        std::cerr << "Can not initialse windows socket." << std::endl;
+        return EXIT_FAILURE;
+    }
+    Socket clientSocket;
+    if ((clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
+        std::cerr << "Can not create client socket." << std::endl;
+        WSACleanup();
+        return EXIT_FAILURE;
+    }
+    SocketAddressIn serverAddress;
+    serverAddress.sin_addr.s_addr = htonl(ServerIP);
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(ServerPort);
+    if (connect(clientSocket, (SocketAddress *) &serverAddress, sizeof(SocketAddress)) == SOCKET_ERROR) {
+        std::cerr << "Can not connet to the server." << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return EXIT_FAILURE;
+    }
+    std::cout << "Successfully connected to the server." << std::endl;
+    char data[MaxDataLength];
+    while (true) {
+        std::cin.getline(data, sizeof(char) * MaxDataLength);
+        if (strcmp(data, "exit") == 0) {
+            break;
+        }
+        if (send(clientSocket, data, strlen(data) + 1, 0) == SOCKET_ERROR) {
+            std::cout << "Can not send data to the server now." << std::endl;
+            closesocket(clientSocket);
+            WSACleanup();
+            return EXIT_FAILURE;
+        }
+    }
+    closesocket(clientSocket);
+    WSACleanup();
+    return EXIT_SUCCESS;
+}
---
+++ server.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <memory>
+#include <cstring>
+#include <windows.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using Socket          = SOCKET;
+using SocketAddress   = SOCKADDR;
+using SocketAddressIn = SOCKADDR_IN;
+using Word            = WORD;
+inline Word makeWord(const int &a, const int &b) {
+    return MAKEWORD(a, b);
+}
+const uint64_t ServerIP      = 0xC0A80106; // or const char *ServerIP = 192.168.1.6, with inet_addr(ServerIP)
+const uint32_t ServerPort    = 10000;
+const uint32_t BackLog       = 10;
+const uint32_t MaxDataLength = MAX_PATH;
+
+int main(int argc, const char *argv[]) {
+    WSAData wsaData;
+    const Word versionRequested = makeWord(2, 2);
+    if (WSAStartup(versionRequested, &wsaData) != 0) {
+        std::cerr << "Can not initialse windows socket." << std::endl;
+        return EXIT_FAILURE;
+    }
+    Socket serverSocket;
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
+        std::cerr << "Can not create server socket." << std::endl;
+        WSACleanup();
+        return EXIT_FAILURE;
+    }
+    SocketAddressIn serverAddress;
+    serverAddress.sin_addr.s_addr = htonl(ServerIP);
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(ServerPort);
+    if (bind(serverSocket, (SocketAddress *) &serverAddress, sizeof(SocketAddress)) == SOCKET_ERROR) {
+        std::cerr << "Can not bind socket with specified address." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return EXIT_FAILURE;
+    }
+    if (listen(serverSocket, BackLog) == SOCKET_ERROR) {
+        std::cerr << "Can not listen on specified socket now." << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return EXIT_FAILURE;
+    }
+    std::cout << "Server successfully launched." << std::endl;
+    Socket clientSocket;
+    SocketAddressIn clientAddress;
+    int length = sizeof(clientAddress);
+    std::vector<std::unique_ptr<std::thread>> threads;
+    while (true) {
+        if ((clientSocket = accept(serverSocket, (SocketAddress *) &clientAddress, &length)) == INVALID_SOCKET) {
+            std::cerr << "Can not accept data from specified socket now." << std::endl;
+            return EXIT_FAILURE;
+        }
+        const short net = clientAddress.sin_addr.s_net;
+        const short host = clientAddress.sin_addr.s_host;
+        const short logicalHost = clientAddress.sin_addr.s_lh;
+        const short imp = clientAddress.sin_addr.s_impno;
+        std::cout << "Connected to " << net << '.' << host << '.' << logicalHost << '.' << imp << '.' << std::endl;
+        threads.emplace_back(new std::thread([clientSocket, net, host, logicalHost, imp, &serverSocket, &threads]() {
+            char data[MaxDataLength];
+            while (true) {
+                memset(data, NULL, sizeof(char) * MaxDataLength);
+                int flag = recv(clientSocket, data, sizeof(char) * MaxDataLength, NULL);
+                if (flag == 0 || flag == SOCKET_ERROR) {
+                    std::cout << "Disconnected from client " << net << '.' << host << '.' << logicalHost << '.' << imp << '.' << std::endl;
+                    closesocket(clientSocket);
+                    return;
+                }
+                if (strcmp(data, "shutdown") == 0) {
+                    closesocket(serverSocket);
+                    for (size_t i = 0; i < threads.size(); i++) {
+                        threads[i]->detach();
+                    }
+                    WSACleanup();
+                    exit(EXIT_SUCCESS);
+                }
+                std::cout << "[" << net << '.' << host << '.' << logicalHost << '.' << imp << "] " << data << std::endl;
+            }
+        }));
+    }
+    return EXIT_SUCCESS;
+}
